397c07b23632368ed8c237148fcf82ce8f0bd7b8
Stefan <stefan.pankowiecki@openstax.pl>
2019-05-15 17:39:00 +0200

    GUI upgrade, db I/O upgrade

---
+++ src/webwviewselenium/SettingsFrame.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webwviewselenium;
+
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+
+/**
+ *
+ * @author stefanmac
+ */
+public class SettingsFrame extends JFrame implements ActionListener {
+
+    
+
+    JButton ConfigPaths = new JButton("Configure Paths");
+    JButton BrowserSelect = new JButton("Browser Select");
+    JButton ClearDB = new JButton("Clear scan Database");
+    JButton Back = new JButton("Back");
+    JButton Uninstall = new JButton("Uninstall");
+    
+    
+
+    public SettingsFrame() {
+        
+        super("WebviewTester");
+        JLayeredPane a = new JLayeredPane();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+        java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) (screenSize.getWidth() / 2);
+        int y = (int) (screenSize.getHeight() / 2);
+        setLocation(x, y);
+        
+        
+        ConfigPaths.addActionListener(this);
+       
+        BrowserSelect.addActionListener(this);
+        Back.addActionListener(this);
+
+        add(ConfigPaths);
+        add(BrowserSelect);
+        add(ClearDB);
+        add(Uninstall);
+        add(Back);
+        
+        setLayout(new GridLayout(5, 1));
+
+      
+        setVisible(true);
+
+    }
+    
+       
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       Object source = e.getSource();
+
+        if (source == Back) {
+           dispose();
+            MainFrame np=new MainFrame();  
+            np.setVisible(true);
+
+       }
+        if (source == ConfigPaths) {
+           dispose();
+            ConfigPathsFrame np=new ConfigPathsFrame();  
+            np.setVisible(true);
+
+       }
+    }}
