8e6a1f2c16ba7368cd7cafa3b8dc7c92ef20c4fb
Stefan <stefan.pankowiecki@openstax.pl>
2019-05-15 17:38:54 +0200

    GUI upgrade, db I/O upgrade

---
+++ src/webwviewselenium/MainFrame.java
@@ -0,0 +1,71 @@
+package webwviewselenium;
+
+import java.awt.Color;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import org.openqa.selenium.Dimension;
+
+public class MainFrame extends JFrame implements ActionListener {
+
+    
+
+    JButton scan = new JButton("Scan");
+    JButton compare = new JButton("Compare");
+    JButton settings = new JButton("Settings");
+    
+    
+
+    public MainFrame() {
+        
+        super("WebviewTester");
+        JLayeredPane a = new JLayeredPane();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+        java.awt.Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) (screenSize.getWidth() / 2);
+        int y = (int) (screenSize.getHeight() / 2);
+        setLocation(x, y);
+        
+        
+        scan.addActionListener(this);
+       
+        compare.addActionListener(this);
+        settings.addActionListener(this);
+
+        add(scan);
+        
+        add(compare);
+        add(settings);
+        
+        setLayout(new GridLayout(3, 1));
+
+      
+        setVisible(true);
+
+    }
+    
+       
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       Object source = e.getSource();
+
+        if (source == settings) {
+           dispose();
+            SettingsFrame np=new SettingsFrame();  //NewJframe is the name of my next Jframe that would be shown
+            np.setVisible(true);
+
+       }
+    }
+
+
+
+}
