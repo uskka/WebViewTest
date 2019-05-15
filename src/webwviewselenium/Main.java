806c8b3a2e74a5ea360a85cc49b7e5d2de315268
Stefan <stefan.pankowiecki@openstax.pl>
2019-05-15 17:38:23 +0200

    GUI upgrade, db I/O upgrade

---
+++ src/webwviewselenium/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webwviewselenium;
+
+import java.awt.EventQueue;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
+import static webwviewselenium.ConsoleHandler.Console;
+
+
+public class Main {
+    
+    	public static void main(String[] args) throws IOException, FileNotFoundException, ParseException {
+
+//Console();
+
+
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+                            new MainFrame();
+                            
+                            
+			}
+		});
+                
+            
+}
+    
+}
+
+
+
+
